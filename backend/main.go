e7961cf34bff33c38cb8aec80eaafb7f3f8fba13
Pranav Kurundodi <111072474+PranavKurundodi@users.noreply.github.com>
2024-04-15 23:11:36 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/handlers"
 
@@ -52,7 +49,7 @@ func main() {
 	pokedexRouter.HandleFunc("/display/{username}", func(w http.ResponseWriter, r *http.Request) {
 		handler.GetUserInventory(w, r, client)
 	}).Methods("GET")
-	r.HandleFunc("/upload", uploadHandler).Methods("POST")
+	r.HandleFunc("/upload", handler.UploadHandler).Methods("POST")
 
 	headers := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
@@ -61,37 +58,4 @@ func main() {
 
 	log.Println("Server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
-}
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the multipart form data
-	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-
-	file, _, err := r.FormFile("image")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	// Create a new file named "model/test.jpg"
-	out, err := os.Create("C:\\Users\\prana\\OneDrive\\Documents\\college\\pokedex\\model\\test.jpg")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusInternalServerError)
-		return
-	}
-	defer out.Close()
-
-	// Copy the uploaded file to the new file
-	_, err = io.Copy(out, file)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("File uploaded successfully"))
 }
