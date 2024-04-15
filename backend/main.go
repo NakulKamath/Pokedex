ae86c6e9a076396e8962e54062924bcdcab13b0a
Pranav Kurundodi <111072474+PranavKurundodi@users.noreply.github.com>
2024-04-15 16:34:09 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PranavKurundodi/pokedex/backend/handlers"
 	"github.com/PranavKurundodi/pokedex/backend/models"
@@ -47,7 +50,42 @@ func main() {
 	pokedexRouter.HandleFunc("/display/{username}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.GetUserInventory(w, r, client)
 	}).Methods("GET")
+	r.HandleFunc("/upload", uploadHandler).Methods("POST")
 
 	log.Println("Server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the multipart form data
+	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Create a new file named "model/test.jpg"
+	out, err := os.Create("C:\\Users\\prana\\OneDrive\\Documents\\college\\pokedex\\model\\test.jpg")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	defer out.Close()
+
+	// Copy the uploaded file to the new file
+	_, err = io.Copy(out, file)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("File uploaded successfully"))
+}
+
